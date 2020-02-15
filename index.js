e8a005c4eccff009b1d9c5936b85037d9c50df04
emma <sonicjane2020@gmail.com>
2020-02-15 03:03:35 -0800

    Added components and properly displayed the data from the joke api. Styled it with a very basic style.

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 axios.get("https://sv443.net/jokeapi/category/Miscellaneous?blacklistFlags=nsfw").then(response => {
-        console.log(response);
-        console.log(response.data.setup);
-        console.log(response.data.delivery);
+
+        const getJokes = newCard(response.data);
+        cards.appendChild(getJokes);
     })
     .catch(error => {
         console.log(error);
     });
 
-    console.log("hi");
+// sets up refresh button
+function refreshPage(){
+    window.location.reload();
+} 
+
+const cards = document.querySelector('.cards');
+
+const newCard = (data) => {
+    const card = document.createElement('div'),
+          jokeContainer = document.createElement('div'),
+          setUp = document.createElement('p'),
+          delivery = document.createElement('p');
+
+    card.classList.add('card');
+    jokeContainer.classList.add('jokeContainer');
+
+    setUp.textContent = data.setup;
+    delivery.textContent = data.delivery;
+
+    card.appendChild(jokeContainer);
+    card.appendChild(setUp);
+    card.appendChild(delivery);
+    
+    return card;
+}
+
